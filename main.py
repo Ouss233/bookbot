149e43b25b41da61c60fd2f3ab76353b03f2eb27
Ouss233 <mg.oussama@gmail.com>
2025-04-03 00:13:20 +0100

    use args

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import count_words
 from stats import count_characters
 from stats import list_sorted
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,11 +10,14 @@ def get_book_text(path_to_file):
 
 
 def main():
+    if (len(sys.argv) !=2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
-    print("Found",count_words(get_book_text('./books/frankenstein.txt')), "total words")
+    print("Found",count_words(get_book_text(sys.argv[1])), "total words")
     #print(count_characters(get_book_text('./books/frankenstein.txt')))
     print("----------- Character Count -----------")
-    report = list_sorted(count_characters(get_book_text('./books/frankenstein.txt')))
+    report = list_sorted(count_characters(get_book_text(sys.argv[1])))
     for element in report:
         if element[0].isalpha() == True:
             print(f"{element[0]}:",element[1])
